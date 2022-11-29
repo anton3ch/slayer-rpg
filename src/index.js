e9ff8a934b714375edaba5e740a076c02fe5419a
Alex Johnson <alex.johnson293@gmail.com>
2022-11-29 15:02:28 -0800

    begin battle integration

--- src/index.js
+++ src/index.js
@@ -3,7 +3,36 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import Character from './js/character.js';
 // import $ from 'jquery'; // $ npm i jquery
+import Entity from './js/entities.js';
+// import Battle from './js/battle.js';
 
+//local store object
+function storeObject(keyword, object){
+  this.sessionStorage.setItem(keyword, JSON.stringify(object));
+}
+
+//retrieve local stored object
+function retrieveObject(keyword){
+  let object = this.sessionStorage.getItem(keyword);
+  object = JSON.parse(object);
+  return object;
+}
+
+
+function battleStart () {
+  retrieveObject("player");
+  //battle logic
+}
+
+//battle event listener
+window.addEventListener("load", function () {
+  this.document.getElementById()
+  let player = new Entity("john");
+  storeObject("player", player);
+});
+
+
+//map event listener
 window.addEventListener("load", function() {
   let char = document.getElementById("char");
 
@@ -14,6 +43,7 @@ window.addEventListener("load", function() {
       document.getElementById("base").setAttribute("class", "hidden");
       document.getElementById("island").removeAttribute("class");
     }, 4000);
+    
   });
 
   char.addEventListener('animationend', function () {
@@ -21,6 +51,7 @@ window.addEventListener("load", function() {
     char.removeAttribute("class", "move");
     char.setAttribute("top", "20px"); // does not work, character just reverts to original position
   });
+
 });
 
 document.getElementById("island").addEventListener('click', function() {
