ad5b47e021591330910db3f9adf5ec055e652de1
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-11-30 16:52:23 -0600

    switchCo-authored-by: Aitana Shough <aeshough@gmail.com>

--- src/index.js
+++ src/index.js
@@ -78,7 +78,6 @@ function battleStart() {
 }
 
 function initiateBattle() {
-  console.log("ello");
   document.getElementById("secondmap").setAttribute("class", "shake");
   setTimeout(function(){
     let char = document.getElementById("char");
@@ -115,7 +114,6 @@ window.addEventListener("load", function() {
 
 // player move to second map
   document.getElementById("bottom-arrow").addEventListener('click', function() {
-    console.log("clickety click")
     char.setAttribute("class", "move");
     setTimeout(() => {
       document.getElementById("base").setAttribute("class", "hidden");
